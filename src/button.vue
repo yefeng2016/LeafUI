62faa1ce029475b7715efd9a618ea2ef004737f4
yefeng <jrtzxh020@foxmail.com>
2019-05-10 00:21:42 +0800

    git

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,41 @@
 <template>
-  <button class="l-button">按钮</button>
+  <button class="l-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
 export default {
-  
+  props: {
+    icon: '',
+    iconPosition: {
+      default: 'left',
+      type: String,
+      
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
   .l-button {
-    height: var(--button-height);
-    border-radius: var(--border-radius);
-    background: var(--button-bg);
-    border: 1px solid var(--border-color);
-    padding: 0 1em;
-    font: inherit;
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
+    height: var(--button-height); border-radius: var(--border-radius); background: var(--button-bg);
+    border: 1px solid var(--border-color); padding: 0 1em; font: inherit;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    &:hover { border-color: var(--border-color-hover); }
+    &:active { background-color: var(--button-active-bg); }
+    &:focus { outline: none; }
+    > .icon { order: 1; margin-right: .2em;}
+    > .content { order: 2; }
+    &.icon-right {
+      > .icon { order: 2; margin-right: 0; margin-left: .2em;  }
+      > .content { order: 1; }
     }
   }
 </style>
