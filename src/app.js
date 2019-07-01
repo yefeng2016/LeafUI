878d35431d0f5e8caa9523470a25c28f57514aa7
yefeng <jrtzxh020@foxmail.com>
2019-07-01 22:49:19 +0800

    删除冗余代码

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 import Vue from 'vue';
 import button from './button.vue';
 import icon from './icon.vue'
-import carousel from './carousel'
-import carouselItem from './carousel-item'
 
 Vue.component('le-button', button)
 Vue.component('le-icon', icon)
 Vue.component('le-carousel', carousel)
-Vue.component('le-carousel-item', carouselItem)
 new Vue({
   el: '#app',
   data: {
